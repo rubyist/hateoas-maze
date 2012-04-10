a0c0cc791ab5a66e41ff865668977b3afaf8b07b
Scott Barron <scott@elitists.net>
2012-04-09 21:56:54 -0400

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HateoasMaze::Application.config.secret_token = '475ce798799104d04f5903bffd39b7369a909e3d857e58208f2ec0e49e24558f59bef13efde1b3480b7625a74d50b3208d60fb94224f6811e89dc18ae3c686cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HateoasMaze::Application.config.session_store :cookie_store, key: '_hateoas-maze_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HateoasMaze::Application.config.session_store :active_record_store
